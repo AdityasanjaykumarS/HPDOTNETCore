64b5ae578efaa7fae7ed1150608cb3b2e68c5309
Administrator <AdityasanjaykumarS@hexaware.com>
2019-10-14 12:43:19 -0400

    created project stature

---
+++ HWResturantCoreSolution/HWResturant.Web/Pages/Resturants.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HWResturant.Web.Pages
+{
+    public class ResturantsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
